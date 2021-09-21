8177b1d7bb2fbebeb1878416d8e689851e030ccb
vamseeuxd <vamsee.uxd@gmail.com>
2021-09-21 08:07:44 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mmm-003',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
