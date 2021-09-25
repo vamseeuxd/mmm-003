c8d06323ffd641f2b19d43ed0ad5d809e8f51883
vamseeuxd <vamsee.uxd@gmail.com>
2021-09-25 07:29:47 +0530

    added @angular/fire

--- src/app/manage-expenses/manage-expenses.page.ts
+++ src/app/manage-expenses/manage-expenses.page.ts
@@ -1,14 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {AngularFirestore} from '@angular/fire/compat/firestore';
-import {switchMap, tap, window} from 'rxjs/operators';
-import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
 import {ModalController} from '@ionic/angular';
 import {TransactionsFormComponent} from './transactions -form/transactions -form.component';
-import * as moment from 'moment';
-import {LoaderService} from '../shared/loader/loader.service';
-import {ordinalSuffixOf} from '../shared/utils/utils';
-import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {TRANSACTION_TYPE, TransactionService} from '../shared/transaction-service/transaction.service';
 
 @Component({
   selector: 'app-folder',
@@ -16,93 +9,26 @@ import {AngularFireAuth} from '@angular/fire/compat/auth';
   styleUrls: ['./manage-expenses.page.scss'],
 })
 export class ManageExpensesPage implements OnInit {
-  public folder: string;
-  typeFilterAction: BehaviorSubject<'expenses' | 'income'> = new BehaviorSubject<'expenses' | 'income'>('expenses');
-  typeFilter$: Observable<'expenses' | 'income'> = this.typeFilterAction.asObservable();
-  selectedDateAction: BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date());
-  selectedDate$: Observable<Date> = this.selectedDateAction.asObservable();
-  dataLoaderId = 0;
-  data$ = combineLatest([this.selectedDate$, this.typeFilter$, this.loader.auth.user]).pipe(
-    switchMap(
-      // eslint-disable-next-line max-len
-      ([selectedDate, type, user]) => {
-        if (!user) {
-          return of([]);
-        }
-        return this.firestore.collection('transactions', ref => {
-          this.dataLoaderId = this.loader.show();
-          // eslint-disable-next-line max-len
-          /* todo: Cloud Firestore Data Modeling (Google I/O'19) : https://www.youtube.com/watch?v=lW7DWV2jST0*/
-          /* todo: Firestore Data Modeling - Five Cool Techniques : https://www.youtube.com/watch?v=35RlydUf6xo*/
-          /*firebase get documents where today is in between to fields - date range (startDate / endDate)*/
-          selectedDate.setDate(1);
-          selectedDate.setHours(0, 0, 0, 0);
-          /*console.log(moment(1609439400000).format('DD-MMMM-yyyy hh-mm-ss'), '>=', moment(selectedDate).format('DD-MMMM-yyyy hh-mm-ss'));
-          console.log(1609439400000, '>=', selectedDate.getTime());*/
-          return ref.where('uid', '==', this.loader.user.providerData[0].uid).where('type', '==', type)
-            .where('dates.end', '>=', selectedDate.getTime());
-        }).valueChanges({idField: 'id'}).pipe(
-          switchMap((transactions: any[]) => {
-              selectedDate.setDate(1);
-              selectedDate.setHours(0, 0, 0, 0);
-              //....
-              return of(
-                transactions.filter(transaction => (transaction.dates.start <= selectedDate.getTime())).map(transaction => ({
-                  ...transaction,
-                  dates: {
-                    start: moment(transaction.dates.start).format('DD-MMMM-yyyy'),
-                    end: moment(transaction.dates.end).format('DD-MMMM-yyyy'),
-                  },
-                  startDate: moment(transaction.startDate).format('DD-MMM-yyyy'),
-                  payments: Array.from(Array(Number(transaction.noOfInstallments)).keys()).map(key => ({
-                    instalment: ordinalSuffixOf(key + 1),
-                    // eslint-disable-next-line max-len
-                    dueDate: moment(transaction.startDate).add(key * transaction.repeatInterval, transaction.repeatOption).format('DD-MMM-yyyy'),
-                    isPaid: false,
-                  }))
-                }))
-              );
-            }
-          ),
-          tap(x => {
-            this.loader.hide(this.dataLoaderId);
-          })
-        );
-      }
-    )
-  );
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private auth: AngularFireAuth,
     public modalController: ModalController,
-    private firestore: AngularFirestore,
-    public loader: LoaderService,
+    public transactionService: TransactionService,
   ) {
   }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-    // @ts-ignore
-    if (window.loader) {
-      // @ts-ignore
-      this.dataLoaderId = window.loader.show();
-    }
-    // this.addNewExpenses('expenses');
   }
 
-  async addNewExpenses(type: 'expenses' | 'income') {
+  async addNewExpenses(type: TRANSACTION_TYPE) {
     const modal = await this.modalController.create({
       component: TransactionsFormComponent,
       backdropDismiss: false,
-      componentProps: {
-        type
-      }
+      componentProps: {type}
     });
     return await modal.present();
   }
 
   onTypeSegmentChange($event: any) {
-    this.typeFilterAction.next(($event.detail.value ? $event.detail.value : 'expenses'));
+    this.transactionService.updateSelectedTransactionType($event.detail.value ? $event.detail.value : 'expenses');
   }
 }
--- src/app/shared/transaction-service/transaction.service.ts
+++ src/app/shared/transaction-service/transaction.service.ts
@@ -1,9 +1,103 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import {LoaderService} from '../loader/loader.service';
+import {switchMap, tap} from 'rxjs/operators';
+import * as moment from 'moment';
+import {ordinalSuffixOf} from '../utils/utils';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export type TRANSACTION_TYPE = 'expenses' | 'income';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+  private transactionTypeAction: BehaviorSubject<TRANSACTION_TYPE> = new BehaviorSubject<TRANSACTION_TYPE>('expenses');
+  private selectedTransactionType$: Observable<TRANSACTION_TYPE> = this.transactionTypeAction.asObservable();
+  private selectedDateAction: BehaviorSubject<Date> = new BehaviorSubject<Date>(new Date());
+  private selectedDate$: Observable<Date> = this.selectedDateAction.asObservable();
+  private dataLoaderId: number;
+  private readonly dateFormat = 'DD-MMMM-yyyy';
+  private transactions$ = combineLatest([
+    this.selectedDate$,
+    this.selectedTransactionType$,
+    this.loader.auth.user
+  ]).pipe(
+    switchMap(
+      ([selectedDate, type, user]) => {
+        if (!user) {
+          return of([]);
+        }
+        return this.firestore.collection('transactions', ref => {
+          this.dataLoaderId = this.loader.show();
+          selectedDate.setDate(1);
+          selectedDate.setHours(0, 0, 0, 0);
+          return ref.where('uid', '==', this.loader.user.providerData[0].uid)
+            .where('type', '==', type)
+            .where('dates.end', '>=', selectedDate.getTime());
+        }).valueChanges({idField: 'id'}).pipe(
+          switchMap((transactions: any[]) => {
+              selectedDate.setDate(1);
+              selectedDate.setHours(0, 0, 0, 0);
+              return of(
+                transactions.filter(transaction => (transaction.dates.start <= selectedDate.getTime()))
+                  .map(transaction => ({
+                    ...transaction,
+                    dates: {
+                      start: moment(transaction.dates.start).format(this.dateFormat),
+                      end: moment(transaction.dates.end).format(this.dateFormat),
+                    },
+                    startDate: moment(transaction.startDate).format(this.dateFormat),
+                    payments: Array.from(Array(Number(transaction.noOfInstallments)).keys()).map(key => ({
+                      instalment: ordinalSuffixOf(key + 1),
+                      dueDate: this.getDueDate(transaction, key),
+                      isPaid: false,
+                    }))
+                  }))
+              );
+            }
+          ),
+          tap(async x => {
+            await this.loader.hide(this.dataLoaderId);
+          })
+        );
+      }
+    )
+  );
+
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore,
+    public loader: LoaderService,
+  ) {
+  }
+
+  updateSelectedTransactionType(type: TRANSACTION_TYPE) {
+    this.transactionTypeAction.next(type);
+  }
+
+  getSelectedTransactionType(): Observable<TRANSACTION_TYPE> {
+    return this.selectedTransactionType$;
+  }
+
+  getTransactions(): Observable<any[]> {
+    return this.transactions$;
+  }
+
+  updateSelectedDateType(date: Date) {
+    this.selectedDateAction.next(date);
+  }
+
+  getSelectedDate(): Observable<Date> {
+    return this.selectedDate$;
+  }
+
+  private getDueDate(transaction: any, index: number) {
+    return moment(transaction.startDate).add(index * transaction.repeatInterval, transaction.repeatOption).format(this.dateFormat);
+  }
+
+
 }
