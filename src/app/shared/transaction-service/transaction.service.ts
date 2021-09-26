3be6ca645cc5700b0a3ef3f7a61d963d637fa19e
vamseeuxd <vamsee.uxd@gmail.com>
2021-09-26 16:47:46 +0530

    added @angular/fire

--- src/app/shared/transaction-service/transaction.service.ts
+++ src/app/shared/transaction-service/transaction.service.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
+import {BehaviorSubject, combineLatest, forkJoin, Observable, of} from 'rxjs';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {AngularFirestore} from '@angular/fire/compat/firestore';
 import {LoaderService} from '../loader/loader.service';
-import {switchMap, tap} from 'rxjs/operators';
+import {switchMap, take, tap} from 'rxjs/operators';
 import * as moment from 'moment';
 import {DurationInputArg1, DurationInputArg2} from 'moment';
 import {ordinalSuffixOf} from '../utils/utils';
@@ -16,10 +16,30 @@ export interface IDates {
   end: string;
 }
 
+/*
+dueDate: 1631298600000
+id: "4JBRcYGWpAaX2EWfC3xz"
+paidOn: 1631298600000
+transactionId: "UDIVsLDfyqkPHZqSKZO6"
+uid: "102322390323163970849"
+*/
+
+export interface IPaymentDoc {
+  dueDate: number;
+  id: string;
+  paidOn: number;
+  transactionId: string;
+  uid: string;
+}
+
 export interface IPayment {
   instalment: string;
+  id: string | null;
   dueDateString: string;
   dueDate: number;
+  paidOn: number | null;
+  transactionId: string;
+  paymentDoc?: IPaymentDoc;
   type: TRANSACTION_TYPE;
   isPaid: boolean;
 }
@@ -37,13 +57,14 @@ export interface ITransactionDoc {
   repeatInterval: number;
   repeatOption: RepeatOption;
   noOfInstallments: number;
+  payments?: IPaymentDoc[];
 }
 
 export interface ITransaction {
   name: string;
   startDate: string;
-  dueDate?: string;
-  isPaid?: boolean;
+  dueDate?: number;
+  isPaid: boolean;
   repeatInterval: number;
   amount: number;
   installment: string;
@@ -68,6 +89,7 @@ export class TransactionService {
   private selectedDate$: Observable<Date> = this.selectedDateAction.asObservable();
   private dataLoaderId: number;
   private readonly dateFormat = 'DD-MMM-yyyy';
+  private payments$: Observable<IPayment[]> = this.firestore.collection<IPayment>('payments').valueChanges({idField: 'id'});
   private transactions$: Observable<ITransaction[]> = combineLatest([
     this.selectedDate$,
     this.selectedTransactionType$,
@@ -86,6 +108,22 @@ export class TransactionService {
             .where('type', '==', type)
             .where('dates.end', '>=', selectedDate.getTime());
         }).valueChanges({idField: 'id'}).pipe(
+          switchMap(x => {
+            const xxx: any = {source: of(x)};
+            x.forEach(value => {
+              xxx[value.id] = this.firestore.collection<IPaymentDoc>('payments', ref => ref.where('transactionId', '==', value.id))
+                .valueChanges({idField: 'id'}).pipe(take(1));
+            });
+            return forkJoin(xxx).pipe(switchMap(
+              (value: any) => {
+                value.source.forEach(d => {
+                  d.payments = value[d.id];
+                  delete value[d.id];
+                });
+                return of(value.source);
+              }
+            ));
+          }),
           switchMap((transactions: ITransactionDoc[]) => {
               selectedDate.setDate(1);
               selectedDate.setHours(0, 0, 0, 0);
@@ -126,6 +164,7 @@ export class TransactionService {
     private firestore: AngularFirestore,
     public loader: LoaderService,
   ) {
+
   }
 
   updateSelectedTransactionType(type: TRANSACTION_TYPE) {
@@ -179,7 +218,7 @@ export class TransactionService {
       return data.dueDate.isBetween(startDate, endDate, undefined, '[]');
     }).map(data => ({
       instalment: data.instalment,
-      dueDate: data.dueDate.format(this.dateFormat)
+      dueDate: data.dueDate.toDate().getTime()
     }));
   }
 
@@ -199,16 +238,25 @@ export class TransactionService {
   }
 
   private getPayments(transaction: ITransactionDoc): IPayment[] {
-    return Array.from(Array(Number(transaction.noOfInstallments)).keys()).map(key => ({
-      instalment: ordinalSuffixOf(key + 1),
-      dueDate: this.getDueDate(transaction, key).toDate().getTime(),
-      dueDateString: this.getDueDate(transaction, key).format(this.dateFormat),
-      type: transaction.type,
-      isPaid: false,
-    }));
+    return Array.from(Array(Number(transaction.noOfInstallments)).keys()).map(key => {
+      const dueDate = this.getDueDate(transaction, key);
+      console.log(transaction.payments.map(d => d.dueDate).includes(dueDate.toDate().getTime()));
+      return ({
+        instalment: ordinalSuffixOf(key + 1),
+        id: null,
+        dueDateString: dueDate.format(this.dateFormat),
+        dueDate: dueDate.toDate().getTime(),
+        paidOn: null,
+        transactionId: transaction.id,
+        type: transaction.type,
+        isPaid: transaction.payments.map(d => d.dueDate).includes(dueDate.toDate().getTime()),
+      });
+    });
   }
 
-  private getTransaction(fireStoreDoc: ITransactionDoc, installment: string, uid: string, dueDate: string = null): ITransaction {
+  private getTransaction(fireStoreDoc: ITransactionDoc, installment: string, uid: string, dueDate: number = null): ITransaction {
+    console.log('---------------------------------');
+    console.log(fireStoreDoc.payments.map(d => d.dueDate).includes(dueDate));
     return {
       amount: fireStoreDoc.amount,
       id: fireStoreDoc.id,
@@ -219,6 +267,7 @@ export class TransactionService {
       type: fireStoreDoc.type,
       fireStoreDoc,
       uid,
+      isPaid: fireStoreDoc.payments.map(d => d.dueDate).includes(dueDate),
       installment,
       dates: this.getDates(fireStoreDoc),
       startDate: moment(fireStoreDoc.startDate).format(this.dateFormat),
