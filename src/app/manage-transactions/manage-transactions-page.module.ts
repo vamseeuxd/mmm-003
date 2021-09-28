91176bfc4fbb5e4bcc5fb5b19579057bf4148d77
vamseeuxd <vamsee.uxd@gmail.com>
2021-09-28 08:32:58 +0530

    added @angular/fire

--- src/app/manage-transactions/manage-transactions-page.module.ts
+++ src/app/manage-transactions/manage-transactions-page.module.ts
@@ -8,18 +8,6 @@ import {ManageTransactionsRoutingModule} from './manage-transactions-routing.mod
 
 import {ManageTransactionsPage} from './manage-transactions.page';
 import {TransactionsFormComponent} from './transactions -form/transactions -form.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTabsModule} from '@angular/material/tabs';
-import {DateNavigatorComponent} from '../shared/date-navigator/date-navigator.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCardModule} from '@angular/material/card';
 import {TransactionsDetailsComponent} from './transactions-details/transactions-details.component';
 import {SharedModule} from '../shared/shared.module';
 
@@ -29,23 +17,10 @@ import {SharedModule} from '../shared/shared.module';
     FormsModule,
     IonicModule,
     ManageTransactionsRoutingModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatDialogModule,
-    MatTabsModule,
-    MatExpansionModule,
     SharedModule,
-    MatCardModule,
   ],
   declarations: [
     ManageTransactionsPage,
-    DateNavigatorComponent,
     TransactionsDetailsComponent,
     TransactionsFormComponent
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,55 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DebounceClickDirective} from './directives/debounce-click/debounce-click.directive';
+import {DateNavigatorComponent} from './date-navigator/date-navigator.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule, MatOptionModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [
-    DebounceClickDirective
+    DebounceClickDirective,
+    DateNavigatorComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatCardModule,
   ],
   exports: [
-    DebounceClickDirective
+    DebounceClickDirective,
+    DateNavigatorComponent,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatCardModule,
   ]
 })
 export class SharedModule {
